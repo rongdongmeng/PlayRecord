e564ccf50939f40b6f3143f8c02c0ff2371806da
rongdm <rongdm@qq.com>
2016-12-12 22:36:43 +0800

    [rong]1.设置gitignore 2.创建tabbar

---
+++ PlayRecord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayRecord
+//
+//  Created by rong on 2016/12/12.
+//  Copyright © 2016年 rong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
